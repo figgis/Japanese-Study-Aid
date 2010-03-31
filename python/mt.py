c64cfe17c95549da256bb197a898a9a86f5bcd25
Fredrik <pi.arctan@gmail.com>
2010-03-31 10:46:43 +0200

    Applied a more pythonic OO-way of implementation.
    .gitattributes tell git that pdf-files are binary and thus
    should be ignored
    
    ignoring all intermediate LaTeX-files

--- python/mt.py
+++ python/mt.py
@@ -11,10 +11,116 @@ out = codecs.getwriter('utf-8')(sys.stdout)
 
 from latex_template import *
 
+#Helper function
+def printheader(word):
+    '''generate a nice header string'''
+    out.write("\n%s\n%s\n" % (word, '-' * len(word)))
+#-------------------------------------------------------------------
+class JPN:
+    '''Super class'''
+    def __init__(self):
+        self.data=[]
+        self.index=0
+        self.iv=[]              # irregular verbs
+        self.uv=[]              # u verbs
+        self.ruv=[]             # ru verbs
+        self.ia=[]              # i adj
+        self.naa=[]             # na adj
 
-# up until the first cell to fill with data
-WIDTH_1ST=29
+    def add(self,x):
+        if x[0]=='irregular':
+            tmp=verb(x[0],x)
+        if x[0]=='u':
+            tmp=verb(x[0],x)
+        if x[0]=='ru':
+            tmp=verb(x[0],x)
+        if x[0]=='na-adjektiv':
+            tmp=naa(x[0],x)
+        if x[0]=='i-adjektiv':
+            tmp=ia(x[0],x)
+        self.data.append(tmp)
 
+    def show(self):
+        for i in self.data:
+            i.show()
+
+    def make_latex(self):
+        out.write(latex_pre)
+
+        l0=['Verb', 'Adjektiv']
+        l1=['Irregular','RU-verbs', 'U-verbs']
+        l2=['irregular', 'ru', 'u']
+        l3=['な-adjektiv', 'い-adjektiv']
+        l4=['na-adjektiv', 'i-adjektiv']
+
+        dic={}
+        dic['Verb']=[l1,l2]
+        dic['Adjektiv']=[l3,l4]
+
+        for sec in l0:      # section
+            make_latex_section(sec)
+            for i,j in zip(dic[sec][0],dic[sec][1]):
+                make_latex_subsection(i)
+                for word in self.data:
+                    if word.type==j:
+                        word.gen_table()
+                out.write(ur'\clearpage')
+
+        out.write(latex_post)
+#-------------------------------------------------------------------
+class Word():                   # base class
+    '''word type'''
+    def __init__(self,type,data):
+        pass
+
+    def show(self):
+        printheader(self.type)
+        for i in self.data:
+            out.write(i)
+            out.write('\n')
+#-------------------------------------------------------------------
+class verb(Word):               # verbs
+    '''derived'''
+    def __init__(self,type,data):
+        self.type=type
+        self.data=data
+
+    def gen_table(self):
+        h=verb_table
+        for i in range(1,len(self.data)):
+            h=h.replace('@'+str(i),self.data[i])
+
+        out.write(h)
+#-------------------------------------------------------------------
+class ia(Word):                 # i adj
+    '''derived'''
+    def __init__(self,type,data):
+        self.type=type
+        self.data=data
+
+    def gen_table(self):
+        h=i_table
+        for i in range(1,len(self.data)):
+            h=h.replace('@'+str(i)+'-',self.data[i])
+
+        out.write(h)
+#-------------------------------------------------------------------
+class naa(Word):                 # na adj
+    '''derived'''
+    def __init__(self,type,data):
+        self.type=type
+        self.data=data
+
+    def gen_table(self):
+        h=na_table
+        for i in range(1,len(self.data)):
+            h=h.replace('@'+str(i),self.data[i])
+
+        h=h.replace('@0',self.data[2][:-1])
+
+        out.write(h)
+
+#-------------------------------------------------------------------
 def width(string):
     return sum(1+(unicodedata.east_asian_width(unicode(c)) in "WF")
         for c in string)
@@ -29,80 +135,6 @@ def pad(array, index, s):
         array[i] = array[i] + ' '*((fill+1)-width(array[i]))
     return fill
 
-def make_table(verb,section=True):
-    '''make complete table'''
-    wc0=pad(verb, [2,4,6,8], u'positive')
-    wc1=pad(verb, [3,5,7,9], u'negative')
-    wch=pad(verb, [1], u'')
-    wcs=width(verb[2])
-    s1=verb[1].encode('utf-8')
-    s2=verb[2].encode('utf-8')
-    s3=verb[3].encode('utf-8')
-    s4=verb[4].encode('utf-8')
-    s5=verb[5].encode('utf-8')
-    s6=verb[6].encode('utf-8')
-    s7=verb[7].encode('utf-8')
-    s8=verb[8].encode('utf-8')
-    s9=verb[9].encode('utf-8')
-    tot= WIDTH_1ST + wc0 + wc1
-
-    if section:
-        print  s2 + '\n' + '-'*wcs
-        print
-
-
-    print  '+' + '-'*(tot+2) + '+' + '\n' +\
-           '| ' + s1 +  ' '*(tot-wch) + '|\n' +\
-           '+------------+------------+' + '-'*(wc0+2) + '+' + '-'*(wc1+2) + '+\n' +\
-           '| Form                    |' + ' Positive ' + ' '*(wc0 - len('positive')) + '|' +\
-           ' Negative ' + ' '*(wc1 - len('positive')) + '|' + '\n' +\
-           '+============+============+' + '='*(wc0+2) + '+' + '='*(wc1+2) + '+' + '\n' +\
-           '|            | Plain      | ' + s2 + '| ' + s3 + '|' + '\n' +\
-           '+ Present    +------------+' + '-'*(wc0+2) + '+' + '-'*(wc1+2) + '+' + '\n' +\
-           '|            | Polite     | ' + s4 + '| ' + s5 + '|' + '\n' +\
-           '+------------+------------+' + '-'*(wc0+2) + '+' + '-'*(wc1+2) + '+' + '\n' +\
-           '|            | Plain      | ' + s6 + '| ' + s7 + '|' + '\n' +\
-           '+ Past       +------------+' + '-'*(wc0+2) + '+' + '-'*(wc1+2) + '+' + '\n' +\
-           '|            | Polite     | ' + s8 + '| ' + s9 + '|' + '\n' +\
-           '+------------+------------+' + '-'*(wc0+2) + '+' + '-'*(wc1+2) + '+' + '\n'
-    print
-
-def make_latex_table(verb):
-    '''latex table'''
-
-    h=verb_table
-
-    for i in range(1,len(verb)):
-        h=h.replace('@'+str(i),verb[i])
-
-    out.write(h)
-
-def make_latex_table_na_adj(verb):
-    '''latex table'''
-
-    h=na_table
-
-    for i in range(1,len(verb)):
-        h=h.replace('@'+str(i),verb[i])
-
-    h=h.replace('@0',verb[2][:-1])
-
-    out.write(h)
-
-def make_latex_table_i_adj(verb):
-    '''latex table'''
-
-    h=i_table
-
-    for i in range(1,len(verb)):
-        h=h.replace('@'+str(i)+'-',verb[i])
-
-    out.write(h)
-
-def print_section_header(s,wc0,wc1,wch,wcs):
-    '''section header'''
-    return s + '\n' + '-'*wcs
-
 def make_latex_section(s):
     '''\section{s}'''
     out.write('\n')
@@ -115,57 +147,18 @@ def make_latex_subsection(s):
     out.write(r'\subsection{%s}' % (s))
     out.write('\n')
 
-def latex_list_all():
-    '''list'''
-    h=r'''
-\begin{table}[ht]
-  begin{tabular}{ll}
-'''
-    make_latex_section('List')
-    print h
-    l=len(list)
-    if l%2!=0:
-        pad=True
-    for i in range(0,l/2):
-        print list[i].encode('utf-8'), '&', list[i+1].encode('utf-8'), '\n'
-    if pad:
-        print '&'
-
-    print r'\end{tabular}'
-    print r'\end{table}'
-
 ###############################################################
 
 pkl_file = open('data.pkl', 'rb')
 list = pickle.load(pkl_file)
 pkl_file.close()
 
-out.write(latex_pre)
-out.flush()
+x=JPN()
+for i in list:
+    x.add(i)
 
-make_latex_section(u'Verb')
-
-for i,j in zip(['Irregular', 'RU-verbs', 'U-verbs'], ['irregular', 'ru', 'u']):
-    make_latex_subsection(i)
-    for k in list:
-        if k[0]==j:
-            make_latex_table(k)
-    out.write(ur'\clearpage')
-    out.flush()
+x.make_latex()
 
 
-make_latex_section(u'Adjektiv')
+# vim: set fileencoding=utf-8  encoding=utf-8 expandtab:
 
-for i,j in zip(['な-adjektiv', 'い-adjektiv'], ['na-adjektiv', 'i-adjektiv']):
-    make_latex_subsection(i)
-    for k in list:
-        if k[0]==j:
-            make_latex_table_na_adj(k)
-        elif k[0]==j:
-            make_latex_table_i_adj(k)
-    out.write(ur'\clearpage')
-    out.flush()
-
-out.write(latex_post)
-
-# vim: set fileencoding=utf-8 : encoding=utf-8
